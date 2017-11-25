cb9a8b2238ea80f43c966bc1906fffb85c95f547
aliErraoui <alierraoui440@gmail.com>
2017-11-25 01:33:31 +0000

    Committtttt

---
+++ DemoFinalHeroku/src/main/java/com/tweets/demo/DemoFinalHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.tweets.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFinalHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFinalHerokuApplication.class, args);
+	}
+}
